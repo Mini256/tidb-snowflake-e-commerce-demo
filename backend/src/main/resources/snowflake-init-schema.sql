3994d25bfc1ae54888e4164f41f29315ef3c5ddb
Mini256 <minianter@foxmail.com>
2022-06-30 10:24:33 +0800

    fix: fix the ddl for snowflake

--- backend/src/main/resources/snowflake-init-schema.sql
+++ backend/src/main/resources/snowflake-init-schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS "hot_items" (
-    "item_id" BIGINT COMMENT 'item id',
-    "item_name" VARCHAR(128) COMMENT 'item name',
-    "item_price" DECIMAL(20,2) COMMENT 'item price',
-    "item_type" VARCHAR(32) COMMENT 'item type',
-    "item_desc" VARCHAR(1024) COMMENT 'item description',
-    "item_label" VARCHAR(32) COMMENT 'item label',
-    PRIMARY KEY ("item_id")
+    item_id BIGINT COMMENT 'item id',
+    item_name VARCHAR(128) COMMENT 'item name',
+    item_price DECIMAL(20,2) COMMENT 'item price',
+    item_type VARCHAR(32) COMMENT 'item type',
+    item_desc VARCHAR(1024) COMMENT 'item description',
+    item_label VARCHAR(32) COMMENT 'item label',
+    PRIMARY KEY (item_id)
 );
 
 CREATE TABLE IF NOT EXISTS "user_labels" (
-    "user_id" VARCHAR(32) COMMENT 'user id',
-    "user_label" VARCHAR(32) COMMENT 'user label',
-    "avg_amount" DECIMAL(20,2) COMMENT 'average amount spent by users',
-    PRIMARY KEY ("user_id")
+    user_id VARCHAR(32) COMMENT 'user id',
+    user_label VARCHAR(32) COMMENT 'user label',
+    avg_amount DECIMAL(20,2) COMMENT 'average amount spent by users',
+    PRIMARY KEY (user_id)
 );
